f0e80ac91b80d1fd669778030fb66aa2238be066
shrowdes <shrowdes@gmail.com>
2014-01-29 17:18:06 +0100

    changement d'archi

---
+++ WEB-INF/classes/bdd/ConnectionMarket.java
@@ -0,0 +1,29 @@
+package bdd;
+
+import javax.naming.*;
+import java.sql.*;
+import javax.sql.*;
+
+public class ConnectionMarket
+{
+  private Connection con;
+
+  public ConnectionMarket(){
+    con = null;
+  }
+
+  public Connection getConnection() throws NamingException,SQLException{
+      Context initCtx = new InitialContext();
+      Context envCtx = (Context) initCtx.lookup("java:comp/env");
+      DataSource ds = (DataSource) envCtx.lookup("marche");
+      con = ds.getConnection();
+
+      return con;
+      }
+
+  public void closeConnection() throws SQLException{
+    if(con != null){
+      con.close();
+    }
+  }
+}
---
+++ WEB-INF/classes/mapping/Offre.java
@@ -0,0 +1,56 @@
+package tools;
+
+public class Offre{
+
+    private int offreId, valeur, quantite, marche, acheteur,acheteurInverse;
+
+    public offre(){}
+
+    public int getOffreId(){
+	return offreId;
+    }
+
+    public void setOffreId(int id){
+	offreId = id;
+    }
+
+    public int getValeur(){
+	return valeur;
+    }
+
+    public void set(int ){
+
+    }
+
+    public int get(){
+
+    }
+
+    public void set(int ){
+
+    }
+
+    public int get(){
+
+    }
+
+    public void set(int ){
+
+    }
+
+    public int get(){
+
+    }
+
+    public void set(int ){
+
+    }
+
+    public int get(){
+
+    }
+
+    public void set(int ){
+
+    }
+}
